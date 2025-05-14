1b4ae6d944572d07bbc0fbbba95a674c019d1668
Clement Ackaah <134501588+kwa4455@users.noreply.github.com>
2025-05-14 10:42:34 +0000

    Update 3_Admin_Tools.py

--- pm25_monitoring_app/pages/3_Admin_Tools.py
+++ pm25_monitoring_app/pages/3_Admin_Tools.py
@@ -9,7 +9,7 @@ from utils import (
     spreadsheet
 )
 from constants import MAIN_SHEET, MERGED_SHEET
-from utils.auth import authenticate_with_google, require_roles,logout_button
+from utils import authenticate_with_google, require_roles,logout_button
 
 # Admin Access Check
 st.title("🔧 Admin Tools")
