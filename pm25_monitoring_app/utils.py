c4a1696db67ccba6a4c595cca1b40b8d1b2ef475
Clement Ackaah <134501588+kwa4455@users.noreply.github.com>
2025-05-19 14:11:46 +0000

    Update utils.py

--- pm25_monitoring_app/utils.py
+++ pm25_monitoring_app/utils.py
@@ -3,7 +3,9 @@ import pandas as pd
 import gspread
 from datetime import datetime
 from oauth2client.service_account import ServiceAccountCredentials
-from st_aggrid import AgGrid, GridOptionsBuilder, GridUpdateMode
+from st_aggrid import AgGrid, GridUpdateMode
+from st_aggrid.grid_options_builder import GridOptionsBuilder
+
 import json
 import time
 from constants import SPREADSHEET_ID, MAIN_SHEET, MERGED_SHEET, CALC_SHEET
