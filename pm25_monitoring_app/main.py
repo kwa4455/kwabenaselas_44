0ef987d875c3f52a3713b097089d846477cc250b
Clement Ackaah <134501588+kwa4455@users.noreply.github.com>
2025-05-21 14:50:13 +0000

    Update main.py

--- pm25_monitoring_app/main.py
+++ pm25_monitoring_app/main.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import sys
 import os
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
 
 from streamlit_option_menu import option_menu
 from auth.login import login_user
